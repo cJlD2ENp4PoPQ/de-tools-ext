40e6622bae76c758ef9925d8e9f071a934ae86df
cJlD2ENp4PoPQ <marvin@popq.de>
2020-06-27 10:10:28 +0200

    add old design and mobile support

--- de.js
+++ de.js
@@ -10,19 +10,20 @@ var deExtension = {
   init: function () {
     var appcontent = document.getElementById("iframe_main_container"); // content
     if (appcontent) {
-      this.saveRace();
+      this.saveRace(false);
       appcontent.addEventListener("load", function (event) {
         deExtension.onPageLoad(event);
       }, true)
       this.addMenuEntries(document);
     } else {
+      this.saveRace(true);
       this.onMobilePageLoad(document)
     }
   },
 
   /**
    * Adds additional menu entries to navigation bar.
-   * @param document document which contains the navigation bar.
+   * @param {Document} document which contains the navigation bar.
    */
   addMenuEntries: function (document) {
     //find any entry first (there isn't any selector to grab the navigation bar itself)
@@ -35,10 +36,10 @@ var deExtension = {
   },
 
   /**
-   * create a new menu entry node.
-   * @param title the title of entry window.
-   * @param url the location of menu content.
-   * @param type the identifier of content-
+   * Create a new menu entry node.
+   * @param {String} title the title of menu entry.
+   * @param {URL} url the location of menu content.
+   * @param {String} type the identifier of content.
    * @returns {HTMLSpanElement}
    */
   createMenuEntry: function (title, url, type) {
@@ -54,13 +55,17 @@ var deExtension = {
 
   /**
    * Content IFrame change listener function.
-   * @param page the page content of content IFrame.
+   * @param {Event} page the page content of content IFrame.
    */
   onPageLoad: function (page) {
     let contentDocument = page.target.contentDocument;
     this.updateExtensions(contentDocument);
   },
 
+  /**
+   * Updates the content frame extensions.
+   * @param {Document} contentDocument the content of the content frame.
+   */
   updateExtensions: function (contentDocument) {
     if (contentDocument.querySelector('form[action="sector.php"]')) {
       SekExtension.onPageLoad(contentDocument);
@@ -85,36 +90,61 @@ var deExtension = {
   },
 
   /**
-   * @param document
+   * Dispatcher for mobile load events.
+   * @param {Document} document the content of the page
    */
   onMobilePageLoad: function (document) {
     let url = document.URL;
     if (url.includes('sector.php')) {
-      this.onPageLoad(document);
+      SekExtension.onPageLoad(document);
+    } else if (url.includes('auction.php')) {
+      TradeExtension.onPageLoad(document);
+    } else if (url.includes('production.php')) {
+      ProductionExtension.onPageLoad(document);
     }
   },
 
-  saveRace: function () {
+  /**
+   * Saves the race of the player.
+   * @param {Boolean} mobile is the mobile page or not.
+   */
+  saveRace: function (mobile) {
     let race;
-    race = document.querySelector("img[src='g/derassenlogo1.png']") ? 'E' : undefined;
-    if(!race) {
-      race = document.querySelector("img[src='g/derassenlogo2.png']") ? 'I' : undefined;
-    }
-    if(!race) {
-      race = document.querySelector("img[src='g/derassenlogo3.png']") ? 'K' : undefined;
-    }
-    if(!race) {
-      race = document.querySelector("img[src='g/derassenlogo4.png']") ? 'Z' : undefined;
+    if(mobile) {
+      let header = document.getElementById('resmain');
+      if(header) {
+        let computedStyle = getComputedStyle(header);
+        if(computedStyle.backgroundImage.includes('3_top.png')) {
+          race = 'K';
+        } else if(computedStyle.backgroundImage.includes('2_top.png')) {
+          race = 'I';
+        } else if (computedStyle.backgroundImage.includes('1_top.png')) {
+          race = 'E';
+        } else if (computedStyle.backgroundImage.includes('4_top.png')){
+          race = 'Z';
+        }
+      }
+    } else {
+      race = document.querySelector("img[src='g/derassenlogo1.png']") ? 'E' : undefined;
+      if(!race) {
+        race = document.querySelector("img[src='g/derassenlogo2.png']") ? 'I' : undefined;
+      }
+      if(!race) {
+        race = document.querySelector("img[src='g/derassenlogo3.png']") ? 'K' : undefined;
+      }
+      if(!race) {
+        race = document.querySelector("img[src='g/derassenlogo4.png']") ? 'Z' : undefined;
+      }
     }
     window.race = race;
   },
 
   /**
    * Event listener for additional menu entries.
-   * @param event the event.
-   * @param title the title of page.
-   * @param url the url of the page.
-   * @param the identifier of content.
+   * @param {Event} event the click event on menu entry.
+   * @param {String} title the title of page.
+   * @param {URL} url the url of the page.
+   * @param {String} type the identifier of content.
    */
   onMenuEntrySelected: function (event, title, url, type) {
     frame.closeIframe();
