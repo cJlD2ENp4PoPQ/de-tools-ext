03ed71ed3f79325d94ea9ae3973c01c8d31fd013
cJlD2ENp4PoPQ <marvin@popq.de>
2021-03-05 15:37:20 +0100

    add article filter trading page

--- js/pages/trade.js
+++ js/pages/trade.js
@@ -4,6 +4,41 @@
  */
 const TradeExtension = {
 
+  optionsArticleArti: [
+    {id: 'art-arti-disabled', value: 'Deaktiviert'},
+    {id: 'art-arti-all', value: 'Nur Artefakte'},
+    {id: 'art-agsora', value: 'Agsora'},
+    {id: 'art-geabwus', value: 'Geabwus'},
+    {id: 'art-auctacon', value: 'Auctacon'},
+    {id: 'art-arta', value: 'Artascendus'},
+    {id: 'art-sekkollus', value: 'Sekkollus'},
+    {id: 'art-pesara', value: 'Pesara'},
+    {id: 'art-geangrus', value: 'Geangrus'},
+    {id: 'art-credit', value: 'Creditrüssel'},
+    {id: 'art-pekasch', value: 'Pekasch'},
+    {id: 'art-vakara', value: 'Vakara'},
+    {id: 'art-empd', value: 'Empdestro'},
+    {id: 'art-recarion', value: 'Recarion'},
+    {id: 'art-empala', value: 'Empala'},
+    {id: 'art-turla', value: 'Turla'},
+    {id: 'art-pekek', value: 'Pekek'},
+    {id: 'art-tronic', value: 'Tronicar'},
+    {id: 'art-feuroka', value: 'Feuroka'},
+    {id: 'art-bloroka', value: 'Bloroka'},
+    {id: 'art-reca', value: 'Recadesto'},
+    {id: 'art-turak', value: 'Turak'},
+    {id: 'art-waringa', value: 'Waringa'},
+  ],
+
+  optionsArticleOther: [
+    {id: 'art-oth-disabled', value: 'Deaktiviert'},
+    {id: 'art-oth-all', value: 'Nur sonstige Waren'},
+    {id: 'art-oth-titan', value: '2 Titanen-Energiekern'},
+    {id: 'art-oth-palenium', value: '500 Palenium'},
+    {id: 'art-oth-tronic', value: '25 Tronic'}
+  ],
+
+
   optionsDE: [
     {id: 'de-disabled', value: 'Deaktiviert'},
     {id: 'de-all', value: 'Nur DE Resourcen'},
@@ -30,6 +65,8 @@ const TradeExtension = {
   ],
   deRes: ['Multiplex', 'Dyharra', 'Iradium', 'Eternium', 'Credits'],
   vsRes: ['Eisen', 'Titan', 'Mexit', 'Dulexit', 'Tekranit', 'Ylesenium', 'Serodium', 'Rowalganium', 'Sextagit', 'Octagium'],
+  artArtis: ['Agsora', 'Geabwus', 'Auctagon', 'Artascendus', 'Sekkollus','Pesara','Geangrus','Creditrüssel','Pekasch','Vakara','Empdestro','Recarion','Empala','Turla','Pekek','Tronicar','Feuroka','Bloroka','Recadesto','Turak','Waringa'],
+  artOthers: ['2 Titanen-Energiekern', '500 Palenium', '25 Tronic'],
 
   storageKey: 'Trade',
 
@@ -73,19 +110,29 @@ const TradeExtension = {
   addFilter: function (content) {
     let deFilter = Storage.getConfig(this.storageKey, 'de-filter');
     let vsFilter = Storage.getConfig(this.storageKey, 'vs-filter');
+    let articleArtiFilter = Storage.getConfig(this.storageKey, 'article-arti-filter');
+    let articleOtherFilter = Storage.getConfig(this.storageKey, 'article-other-filter');
     let selectDe = fields.createSelectField('de-filter', this.optionsDE, this.onChange, deFilter);
     let selectVs = fields.createSelectField('vs-filter', this.optionsVS, this.onChange, vsFilter);
+    let selectArti = fields.createSelectField('article-arti-filter', this.optionsArticleArti, this.onChange, articleArtiFilter);
+    let selectOther = fields.createSelectField('article-other-filter', this.optionsArticleOther, this.onChange, articleOtherFilter);
     let deField = fields.createField(selectDe);
     let vsField = fields.createField(selectVs);
-    let row = fields.createRow([deField, vsField]);
-    let label = content.createElement('span');
-    label.classList.add('filter_label');
-    label.innerText = 'Währung';
-    let fieldset = fields.createFieldset('Filter', [label, row]);
+    let artiField = fields.createField(selectArti);
+    let otherField = fields.createField(selectOther);
+    let rowRess = fields.createRow([deField, vsField]);
+    let rowArticles = fields.createRow([artiField, otherField]);
+    let labelRess = content.createElement('span');
+    labelRess.classList.add('filter_label');
+    labelRess.innerText = 'Währung';
+    let labelArticles = content.createElement('span');
+    labelArticles.classList.add('filter_label');
+    labelArticles.innerText = 'Ware';
+    let fieldsetRess = fields.createFieldset('Filter', [labelRess, rowRess, labelArticles, rowArticles]);
 
     let target = content.querySelector('div');
     let before = content.querySelector('table');
-    target.insertBefore(fieldset, before);
+    target.insertBefore(fieldsetRess, before);
     if (deFilter) {
       let option = selectDe.querySelector('#' + deFilter);
       this.filterDEEntries(deFilter, option.innerText, content);
@@ -94,18 +141,34 @@ const TradeExtension = {
       let option = selectVs.querySelector('#' + vsFilter);
       this.filterVSEntries(vsFilter, option.innerText, content);
     }
+    if (articleArtiFilter) {
+      let option = selectDe.querySelector('#' + deFilter);
+      this.filterArticleArtiEntries(articleArtiFilter, option.innerText, content)
+    }
+    if (articleOtherFilter) {
+      let option = selectDe.querySelector('#' + deFilter);
+      this.filterArticleArtiEntries(articleOtherFilter, option.innerText, content)
+    }
   },
 
   onChange: function (event) {
     let filterId = event.target.id;
     let ownerDocument = event.target.ownerDocument;
     let selectedOption = event.target.selectedOptions[0];
-    ownerDocument.querySelectorAll('tr.disabled').forEach(tr => tr.classList.remove('disabled'));
     if (filterId === 'de-filter') {
+      ownerDocument.querySelectorAll('tr.disabled-currency').forEach(tr => {tr.classList.remove('disabled-currency')});
       TradeExtension.filterDEEntries(selectedOption.id, selectedOption.innerText, ownerDocument);
     } else if (filterId === 'vs-filter') {
+      ownerDocument.querySelectorAll('tr.disabled-currency').forEach(tr => {tr.classList.remove('disabled-currency')});
       TradeExtension.filterVSEntries(selectedOption.id, selectedOption.innerText, ownerDocument);
+    } else if(filterId === 'article-arti-filter') {
+      ownerDocument.querySelectorAll('tr.disabled-article').forEach(tr => {tr.classList.remove('disabled-article')});
+      TradeExtension.filterArticleArtiEntries(selectedOption.id, selectedOption.innerText, ownerDocument);
+    } else if(filterId === 'article-other-filter') {
+      ownerDocument.querySelectorAll('tr.disabled-article').forEach(tr => {tr.classList.remove('disabled-article')});
+      TradeExtension.filterArticleOtherEntries(selectedOption.id, selectedOption.innerText, ownerDocument);
     }
+
   },
 
   /**
@@ -124,10 +187,10 @@ const TradeExtension = {
       rows.forEach(row => {
         if (filterEntryID === 'vs-all') {
           if (new RegExp(TradeExtension.deRes.join("|")).test(row.children[1].innerText)) {
-            row.classList.add('disabled');
+            row.classList.add('disabled-currency');
           }
         } else if (!new RegExp(filterText).test(row.children[1].innerText)) {
-          row.classList.add('disabled');
+          row.classList.add('disabled-currency');
         }
       })
     }
@@ -149,10 +212,60 @@ const TradeExtension = {
       rows.forEach(row => {
         if (filterEntryID === 'de-all') {
           if (new RegExp(TradeExtension.vsRes.join("|")).test(row.children[1].innerText)) {
-            row.classList.add('disabled');
+            row.classList.add('disabled-currency');
           }
         } else if (!new RegExp(filterText).test(row.children[1].innerText)) {
-          row.classList.add('disabled');
+          row.classList.add('disabled-currency');
+        }
+      })
+    }
+  },
+
+  /**
+   * Filters the auction page entries by given DE article filter parameter
+   * @param {String} filterEntryID
+   * @param {String} filterText
+   * @param {Document} document
+   */
+  filterArticleArtiEntries: function (filterEntryID, filterText, document) {
+    Storage.storeConfig(TradeExtension.storageKey, 'article-arti-filter', filterEntryID);
+    Storage.removeConfig(TradeExtension.storageKey, 'article-other-filter');
+    let otherFilter = document.getElementById('article-other-filter');
+    otherFilter.value = 'art-oth-disabled';
+    let rows = document.querySelectorAll('tr[style="text-align: right; vertical-align: middle;"]');
+    if (filterEntryID !== 'art-arti-disabled') {
+      rows.forEach(row => {
+        if (filterEntryID === 'art-arti-all') {
+          if (new RegExp(TradeExtension.artOthers.join("|")).test(row.children[0].innerText)) {
+            row.classList.add('disabled-article');
+          }
+        } else if (!new RegExp(filterText).test(row.children[0].innerText)) {
+          row.classList.add('disabled-article');
+        }
+      })
+    }
+  },
+
+  /**
+   * Filters the auction page entries by given DE article filter parameter
+   * @param {String} filterEntryID
+   * @param {String} filterText
+   * @param {Document} document
+   */
+  filterArticleOtherEntries: function (filterEntryID, filterText, document) {
+    Storage.storeConfig(TradeExtension.storageKey, 'article-other-filter', filterEntryID);
+    Storage.removeConfig(TradeExtension.storageKey, 'article-arti-filter');
+    let artiFilter = document.getElementById('article-arti-filter');
+    artiFilter.value = 'art-arti-disabled';
+    let rows = document.querySelectorAll('tr[style="text-align: right; vertical-align: middle;"]');
+    if (filterEntryID !== 'art-oth-disabled') {
+      rows.forEach(row => {
+        if (filterEntryID === 'art-oth-all') {
+          if (new RegExp(TradeExtension.artArtis.join("|")).test(row.children[0].innerText)) {
+            row.classList.add('disabled-article');
+          }
+        } else if (!new RegExp(filterText).test(row.children[0].innerText)) {
+          row.classList.add('disabled-article');
         }
       })
     }
