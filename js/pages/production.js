001408eeaaaa17413345da43db3ae4c26ddf8e21
cJlD2ENp4PoPQ <cjld2enp4popq@popq.de>
2025-10-10 18:15:09 +0200

    feat: display of support values

--- js/pages/production.js
+++ js/pages/production.js
@@ -3,8 +3,108 @@
  * @type {{onPageLoad: ProductionExtension.onPageLoad, update: ProductionExtension.update, addResDistrKey: ProductionExtension.addResDistrKey, createTd: (function(String, String): HTMLTableDataCellElement)}}
  */
 const ProductionExtension = {
+
+  supportValues: {
+      'E': {
+        1: {
+          4: 8,
+          5: 18
+        },
+        3: {
+          5: 5
+        },
+        4: {
+          5: 3
+        }
+      },
+      'K': {
+        1: {
+          4: 7,
+          5: 14
+        },
+        3: {
+          5: 5
+        },
+        4: {
+          5: 3
+        }
+      },
+      'I': {
+        1: {
+          4: 8,
+          5: 18
+        },
+        3: {
+          5: 5
+        },
+        4: {
+          5: 3
+        }
+      },
+      'Z': {
+        1: {
+          4: 12,
+          5: 30
+        },
+        3: {
+          5: 5
+        },
+        4: {
+          5: 3
+        }
+      }
+    },
+
+  blockSupportValues: {
+    'E': {
+      4: {
+        2: 10
+      },
+      5: {
+        2: 22
+      },
+      8: {
+        2: 34
+      }
+    },
+    'K': {
+      4: {
+        2: 6
+      },
+      5: {
+        2: 14
+      },
+      8: {
+        2: 26
+      }
+    },
+    'I': {
+      4: {
+        2: 12
+      },
+      5: {
+        2: 28
+      },
+      8: {
+        2: 43
+      }
+    },
+    'Z': {
+      4: {
+        2: 12
+      },
+      5: {
+        2: 29
+      },
+      8: {
+        2: 48
+      }
+    }
+  },
+
   onPageLoad: function (content) {
     this.addResDistrKey(content);
+    this.addSupportValues(content);
   },
 
   /**
@@ -25,6 +125,69 @@ const ProductionExtension = {
     tr.insertBefore(this.createTd('e',"0%"), null);
     let lastRow = resultTable.childNodes[resultTable.childElementCount];
     resultTable.insertBefore(tr, lastRow);
+  },
+
+  /**
+   * Add the minimum required ship amount to support other ship types.
+   * @param {Document} content the document which contains the production form.
+   */
+  addSupportValues: function (content) {
+    const resultTable = content.querySelector('form[action="production.php"] table:nth-child(1) tr td table tbody');
+    const shipTypeRows = resultTable.querySelectorAll('tr');
+    const raceSupportValues = this.supportValues[window.race];
+    Object.entries(raceSupportValues).forEach(([type, supportReq]) => {
+      const intType = parseInt(type)
+      const supportGivingShipTypeRow = shipTypeRows[intType];
+      let shipsNeeded = 0;
+      let cellClasses = []
+      const supportGivingAmountNode = supportGivingShipTypeRow.childNodes[supportGivingShipTypeRow.childElementCount - 2];
+      const supportGivingShips = parseInt(supportGivingAmountNode.innerText.replace(/\./g, ''));
+      Object.entries(supportReq).forEach(([type, amount]) => {
+        const intType = parseInt(type);
+        const shipTypeNeedingSupportRow = shipTypeRows[intType];
+        let shipAmountNode = shipTypeNeedingSupportRow.childNodes[shipTypeNeedingSupportRow.childElementCount - 2];
+        const shipsNeedingSupport = parseInt(shipAmountNode.innerText.replace(/\./g, ''));
+        cellClasses = shipAmountNode.classList
+        shipsNeeded += shipsNeedingSupport * amount
+      })
+      let cellStyle = undefined;
+      if (supportGivingShips >= shipsNeeded) {
+        cellStyle = 'color: #00ff00'
+      } else {
+        cellStyle = 'color: #ff0000'
+      }
+      this.addSupportRow(resultTable, supportGivingShipTypeRow.nextElementSibling, cellClasses, shipsNeeded, cellStyle);
+    })
+  },
+
+  /**
+   * Add a new row to display the needed ship amount below the ship type.
+   * @param {HTMLElement} content the form with all ships
+   * @param {HTMLElement} beforeNode the row after the support requiring row
+   * @param {string} cellClass the cell class of the row
+   * @param {number} neededSupport the minimum needed amount of the ship type
+   * @param {string} cellStyle additional styling
+   */
+  addSupportRow: function (content, beforeNode, cellClass, neededSupport, cellStyle) {
+    const tr = document.createElement('tr');
+    tr.setAttribute('valign', 'middle');
+    tr.setAttribute('align', 'center');
+    tr.setAttribute('height', '25');
+
+    const tdText = document.createElement('td');
+    tdText.classList.add(cellClass);
+    tdText.innerText = "Benötigter Support durch Großkampfschiffe"
+    tdText.style = cellStyle;
+    tdText.setAttribute('colspan', '7');
+    tr.insertBefore(tdText, null);
+
+    const tdValue = document.createElement('td');
+    tdValue.classList.add(cellClass);
+    tdValue.innerText = "" + neededSupport;
+    tdValue.style = cellStyle;
+    tr.insertBefore(tdValue, null);
+
+    content.insertBefore(tr, beforeNode);
   },
 
   /**
